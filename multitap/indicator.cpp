ac1800d79ed1b179ef3a28300260a5db5be8dd2f
SonicFreak94 <sonicfreak94@hotmail.com>
2016-01-04 18:45:18 -0700

    moved things to mod loader and renamed things

--- multitap/indicator.cpp
+++ multitap/indicator.cpp
@@ -1,12 +1,6 @@
 #include <SADXModLoader.h>
 
-FunctionPointer(void, Draw2DSprite, (NJS_SPRITE *_sp, Int n, Float pri, Uint32 attr, char zfunc_type), 0x00404660);
-FunctionPointer(void, SetSpriteColor, (NJS_ARGB *a1), 0x00402F40);
-FunctionPointer(Sint32, njLoadTexture, (NJS_TEXLIST *texlist), 0x0077FC80);
-FunctionPointer(Bool, IsControllerEnabled, (Uint8 index), 0x0040EFD0);
-FunctionPointer(char, IsVisible, (NJS_VECTOR *v), 0x004CD730);
-
-NJS_TEXNAME multicommon_texname[] = {
+NJS_TEXNAME multicommon_TEXNAME[] = {
 	{ "arrow",	NJD_TEXATTR_BOTH, 0 },
 	{ "cpu_1",	NJD_TEXATTR_BOTH, 0 },
 	{ "cpu_2",	NJD_TEXATTR_BOTH, 0 },
@@ -14,12 +8,14 @@ NJS_TEXNAME multicommon_texname[] = {
 	{ "p1",		NJD_TEXATTR_BOTH, 0 },
 	{ "p2",		NJD_TEXATTR_BOTH, 0 },
 	{ "p3",		NJD_TEXATTR_BOTH, 0 },
-	{ "p4",		NJD_TEXATTR_BOTH, 0 },
+	{ "p4",		NJD_TEXATTR_BOTH, 0 }
 };
 
-NJS_TEXLIST multicommon_texlist = { arrayptrandlength(multicommon_texname) };
+NJS_TEXLIST multicommon_TEXLIST = { arrayptrandlength(multicommon_TEXNAME) };
 
-NJS_TEXANIM multicommon_texanim[] = {
+NJS_TEXANIM Indicator_TEXANIM[] = {
+	// w,	h,	cx,	cy,	u1,	v1,	u2,		v2,	texid,	attr
+	// u2 and v2 must be 0xFF
 	{ 24,	16,	12,	16,	0,	0,	0xFF,	0xFF,	0,	0 },	// arrow
 	{ 24,	24,	24,	40,	0,	0,	0xFF,	0xFF,	1,	0 },	// cpu_1
 	{ 24,	24,	0,	40,	0,	0,	0xFF,	0xFF,	2,	0 },	// cpu_2
@@ -30,7 +26,7 @@ NJS_TEXANIM multicommon_texanim[] = {
 	{ 24,	24,	0,	40,	0,	0,	0xFF,	0xFF,	7,	0 }		// p4
 };
 
-NJS_SPRITE multicommon_sprite = { { 0.f, 0.f, 0.f }, 1.f, 1.f, 0, &multicommon_texlist, multicommon_texanim };
+NJS_SPRITE Indicator_SPRITE = { { 0.0f, 0.0f, 0.0f }, 1.0f, 1.0f, 0, &multicommon_TEXLIST, Indicator_TEXANIM };
 
 NJS_ARGB colors[] = {
 	{ 1.000f, 0.000f, 0.000f, 1.000f }, // Sonic
@@ -42,7 +38,7 @@ NJS_ARGB colors[] = {
 	{ 1.000f, 0.545f, 0.545f, 0.545f }, // Gamma
 	{ 1.000f, 0.451f, 0.192f, 0.804f }, // Big
 	{ 1.000f, 0.000f, 1.000f, 1.000f }, // Metal Sonic
-	{ 0.750f, 0.500f, 0.500f, 0.500f }
+	{ 0.750f, 0.500f, 0.500f, 0.500f }	// CPU
 };
 
 enum AnimIndex
@@ -53,7 +49,7 @@ enum AnimIndex
 
 void LoadIndicators()
 {
-	LoadPVM("multicommon", &multicommon_texlist);
+	LoadPVM("multicommon", &multicommon_TEXLIST);
 }
 
 void DrawElement(Uint32 i, Uint32 n)
@@ -63,7 +59,7 @@ void DrawElement(Uint32 i, Uint32 n)
 	if (player == nullptr)
 		return;
 
-	NJS_SPRITE* sp = &multicommon_sprite;
+	NJS_SPRITE* sp = &Indicator_SPRITE;
 	NJS_VECTOR pos = player->Position;
 	Uint8 charid = (MetalSonicFlag) ? Characters_MetalSonic : player->CharID;
 	pos.y += PhysicsArray[charid].CollisionSize;
@@ -81,14 +77,14 @@ void DrawElement(Uint32 i, Uint32 n)
 	}
 	else
 	{
-		SetSpriteColor(IsControllerEnabled(i) ? &colors[charid] : &colors[9]);
-		Draw2DSprite(&multicommon_sprite, n, -1.0f, NJD_SPRITE_COLOR | NJD_SPRITE_ALPHA, 0);
+		SetSpriteColor(IsControllerEnabled((Uint8)i) ? &colors[charid] : &colors[9]);
+		Draw2DSprite(&Indicator_SPRITE, n, -1.0f, NJD_SPRITE_COLOR | NJD_SPRITE_ALPHA, 0);
 	}
 }
 
 void DrawIndicators()
 {
-	njSetTexture(&multicommon_texlist);
+	njSetTexture(&multicommon_TEXLIST);
 	njSetTextureNum(arrow);	// arrow
 
 	for (Uint32 i = 0; i < 4; i++)
@@ -96,14 +92,14 @@ void DrawIndicators()
 
 	for (Uint32 i = 0; i < 4; i++)
 	{
-		AnimIndex a = IsControllerEnabled(i) ? p : cpu_1;
+		AnimIndex a = IsControllerEnabled((Uint8)i) ? p : cpu_1;
 		njSetTextureNum(a);
 		DrawElement(i, a);
 	}
 
 	for (Uint32 i = 0; i < 4; i++)
 	{
-		AnimIndex a = IsControllerEnabled(i) ? (AnimIndex)(p1 + i) : cpu_2;
+		AnimIndex a = IsControllerEnabled((Uint8)i) ? (AnimIndex)(p1 + i) : cpu_2;
 		njSetTextureNum(a);
 		DrawElement(i, a);
 	}
